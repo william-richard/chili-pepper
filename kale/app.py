6e635326211a56271d2a9870e0ed4ec97dbdab54
William Richard <william.richard.no.s@gmail.com>
2019-04-08 01:03:57 +0000

    Some basic functionality working
    - actually create the cloudformation stack
    - decorator function wrapping works correctly while being a method of the Kale class
    - added logging to the deployment cli command

--- kale/app.py
+++ kale/app.py
@@ -21,7 +21,6 @@ class Kale:
         self._bucket_name = bucket_name
         self._runtime = runtime
         self._task_functions = list()
-        self._function_handles = list()
 
     @property
     def app_name(self):
@@ -39,56 +38,54 @@ class Kale:
         return self._runtime
 
     @property
-    def function_handles(self):
-        # type: () -> List[str]
-        return self._function_handles
-
-    @property
     def task_functions(self):
         # type: () -> List[builtins.function]
         return self._task_functions
 
-    def task(self, func):
-        # Ensure that the function signature matches what lambda expects
-        # otherwise it will not be callabale from lambda
-        # https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html
-        # TODO make this cloud-agnostic
-        try:
-            function_signature = inspect.signature(func)
-            function_parameter_list = list(function_signature.parameters.keys())
-        except AttributeError:
-            # python2.7 has different inspect module
-            function_arg_spec = inspect.getargspec(func)
-            function_parameter_list = list(function_arg_spec.args)
-        if function_parameter_list != ["event", "context"]:
-            raise InvalidFunctionSignature(
-                "Kale requires that you task functions has 2 parameters - 'event' and 'context' to match what Lambda expects. Your function "
-                + func.__module__
-                + "."
-                + func.__name__
-                + " has these parameters: "
-                + str(function_parameter_list)
-            )
+    def task(self):
+        def _decorator(func):
+            # Ensure that the function signature matches what lambda expects
+            # otherwise it will not be callabale from lambda
+            # https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html
+            # TODO make this cloud-agnostic
+            try:
+                function_signature = inspect.signature(func)
+                function_parameter_list = list(function_signature.parameters.keys())
+            except AttributeError:
+                # python2.7 has different inspect module
+                function_arg_spec = inspect.getargspec(func)
+                function_parameter_list = list(function_arg_spec.args)
+            if function_parameter_list != ["event", "context"]:
+                raise InvalidFunctionSignature(
+                    "Kale requires that you task functions has 2 parameters - 'event' and 'context' to match what Lambda expects. Your function "
+                    + func.__module__
+                    + "."
+                    + func.__name__
+                    + " has these parameters: "
+                    + str(function_parameter_list)
+                )
 
-        self._task_functions.append(func)
+            self._task_functions.append(func)
 
-        def _delay_wrapper(event):
-            # see https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html
-            # the delay function arguments must be just the event argument
-            # context is added by lambda
-            """
-            plan of attack
-            1) Compute or look up the function name
-            2) call https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lambda.html#Lambda.Client.invoke
-            3) return a wrapper of the response, payload, logs, etc
+            def _delay_wrapper(event):
+                # see https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html
+                # the delay function arguments must be just the event argument
+                # context is added by lambda
+                """
+                plan of attack
+                1) Compute or look up the function name
+                2) call https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lambda.html#Lambda.Client.invoke
+                3) return a wrapper of the response, payload, logs, etc
 
-            The easy way would be to set InvocationType to Event, and look up the return value some other way...
-            but the docs don't seem to explain how to do that.  AWS docs often are not very clear though.
+                The easy way would be to set InvocationType to Event, and look up the return value some other way...
+                but the docs don't seem to explain how to do that.  AWS docs often are not very clear though.
 
-            If the Event InvocationType don't work, we might need call invoke in a separate thread/process
-            and return a generator that can wait until the synchronous call to lambda completes and returns everything.
-            """
-            pass
+                If the Event InvocationType don't work, we might need call invoke in a separate thread/process
+                and return a generator that can wait until the synchronous call to lambda completes and returns everything.
+                """
+                pass
 
-        func.delay = _delay_wrapper
-        return func
+            func.delay = _delay_wrapper
+            return func
+
+        return _decorator
--- tests/functional/basicapp/kale_demo.py
+++
@@ -1,8 +0,0 @@
-from kale.app import Kale
-
-app = Kale(app_name="demo", bucket_name="TODO", runtime="python3.7")
-
-
-@app.task()
-def say_hello():
-    print("Hello world!")
